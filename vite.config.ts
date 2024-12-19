c8194769bb4a21488b9b2e3443d9dc0a9bab0839
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2024-12-19 19:01:00 +0530

    deploy react to github

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Smart_calculator/",
   plugins: [react()],
   envDir: '.',
   define: {
